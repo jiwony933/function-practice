e2ccfdf7cecff5a2cb6da2c817a053634f27d2be
jiwon kim <jiwon@jiwonui-MacBookAir.local>
2022-05-28 11:52:38 +0900

    변수가 있는 function 연습

---
+++ 0527/script.js
@@ -0,0 +1,7 @@
+function handleClick() {
+  alert("hello!");
+}
+function handleClick2() {
+  document.getElementsByClassName("my-button2")[0].style.color = "red";
+  document.getElementsByClassName("my-button2")[0].style.fontSize = "25px";
+}
